47a659210127f08d8de1f6fec34ebec3d75800c9
divyanibhusari <divyanibhusari79@gmail.com>
2026-09-16 11:27:21 +0530

    this is a code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { motion, useScroll, useTransform } from "framer-motion"
-import video from "../assets/herosection/Projectvideo1.mp4"
+import video from "../assets/herosection/Projectvideo.mp4"
 
 const Home = () => {
   const videoRef = useRef(null)
